7639f18b6d3f68dd21ff652cb4119e2226ed5d1d
neiled <neil@plasticwater.com>
2011-01-25 19:19:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triptracker::Application.config.secret_token = '825ac352a741568a1986da90f82bbf0e570fe2eb8ddece6baa47b6ce782eaf26a1c03d0fbc9ce48334f5d7a795cd4b56f3b473b5767ad76830a5a50e797a4db2'
